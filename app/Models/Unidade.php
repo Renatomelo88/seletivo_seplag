10c1fc18da610d9d870b8c8f087e0dc29750cae6
Renato Melo <renatomelo@unemat.br>
2025-03-28 22:46:06 -0400

    adiciona relacionamento de endereço na unidade

--- app/Models/Unidade.php
+++ app/Models/Unidade.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 class Unidade extends ModelBase
 {
     protected $table = 'unidade';
@@ -9,5 +11,8 @@ class Unidade extends ModelBase
         'nome',
         'sigla',
     ];
+    public function endereco(): BelongsToMany{
+        return $this->belongsToMany(Endereco::class, 'unidade_endereco', 'unidade_id', 'endereco_id');
+    }
 
 }
