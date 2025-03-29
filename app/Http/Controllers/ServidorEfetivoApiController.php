202b2817e918fa9d66736c53d9e95613d75f17e2
Renato Melo <renatomelo@unemat.br>
2025-03-28 22:18:37 -0400

    Revert "renomeia ServidorEfetivoApiController.php para ServidorEfetivoController.php"
    This reverts commit 0afaa487c880ea4bf2f9c1139eb7795aebcf6754.

--- app/Http/Controllers/ServidorEfetivoApiController.php
+++ app/Http/Controllers/ServidorEfetivoApiController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class ServidorEfetivoController extends ApiController
+class ServidorEfetivoApiController extends ApiController
 {
     public function index(Request $request)
     {
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ServidorEfetivoController;
+use App\Http\Controllers\ServidorEfetivoApiController;
 
 Route::post('/login', [LoginController::class, 'login']);
 
@@ -11,9 +11,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/renova-token', [LoginController::class, 'renovaToken']);
 
-    Route::apiResource('/servidor-efetivo', ServidorEfetivoController::class);
-
-    Route::apiResource('/unidade', ServidorEfetivoController::class);
+    Route::apiResource('/servidor-efetivo', ServidorEfetivoApiController::class);
 
 
 });
